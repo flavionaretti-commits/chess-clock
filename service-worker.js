a76a20d6be446e45ad5bfda07eed8cac8b221930
Shankh <flavio.naretti@gmail.com>
2026-09-13 00:06:50 +0200

    Corregge installazione e isolamento PWA

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 const CACHE = 'chess-clock-pwa-v2';
+const CACHE_PREFIX = 'chess-clock-pwa-';
 const ASSETS = [
   './',
   './index.html',
@@ -15,7 +16,7 @@ self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith(CACHE_PREFIX) && k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', event => {
   if(event.request.method !== 'GET') return;
